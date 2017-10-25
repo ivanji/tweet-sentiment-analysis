662d1cf75b113a6e9a20db17909983cf22efd329
Ivan J <ivanji@gmail.com>
2017-10-25 21:26:02 +1100

    Initial commit of basic script.

---
+++ ngb-sentiment.py
@@ -0,0 +1,31 @@
+import tweepy
+from textblob import TextBlob
+
+# Step 1 - Authenticate
+consumer_key= 'XXX'
+consumer_secret= 'XXX'
+
+access_token='9349572-XX'
+access_token_secret='XXX'
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+#Step 3 - Retrieve Tweets
+public_tweets = api.search('rbainfo')
+
+
+#CHALLENGE - Instead of printing out each tweet, save each Tweet to a CSV file
+#and label each one as either 'positive' or 'negative', depending on the sentiment
+#You can decide the sentiment polarity threshold yourself
+
+
+for tweet in public_tweets:
+    print(tweet.text)
+
+    #Step 4 Perform Sentiment Analysis on Tweets
+    analysis = TextBlob(tweet.text)
+    print(analysis.sentiment)
+    #print("")
